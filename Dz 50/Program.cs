bc89b38f262b32ff64ab8977f89f081f5b6c0071
Ilia <polikarpovilja@mail.ru>
2022-12-12 22:51:21 +0300

    newone

---
+++ Dz 50/Program.cs
@@ -0,0 +1,66 @@
+﻿int m = new Random().Next(2, 15);                   
+int n = new Random().Next(2, 15);
+
+int row = GetUserNumber("Введите номер строки элемента: ", "Ошибка ввода!");
+int column = GetUserNumber("Введите номер столбца элемента: ", "Ошибка ввода!");
+
+
+double[,] array = GetArray(m, n, -100, 100);       
+PrintArray(array);
+string result = CheckingIfElementExists(array, row, column);
+Console.WriteLine("----------------------------------");
+Console.WriteLine(result);
+
+int GetUserNumber(string messageToUser, string errorMessage)
+{
+    while (true)
+    {
+        Console.Write(messageToUser);
+        if (int.TryParse(Console.ReadLine(), out int userNumber))
+            return userNumber;
+        Console.WriteLine(errorMessage);
+    }
+}
+
+double[,] GetArray (int m, int n, int minValue, int maxValue)
+{
+    double[,] resultArray = new double[m,n];
+    for (int i =0; i< m; i++)
+    {
+        for (int j=0; j < n; j++)
+        {
+           double result = new Random().Next(minValue, maxValue + 1) + new Random().NextDouble();
+           resultArray[i, j] = Math.Round(result, 2); 
+        }
+    }
+    return resultArray;
+}
+
+void PrintArray(double[,] inArray)
+{
+    for (int i = 0; i < inArray.GetLength(0); i++)
+    {
+        for (int j = 0; j < inArray.GetLength(1); j++)
+        {
+            Console.Write($"{inArray[i, j]}  ");
+        }
+        Console.WriteLine();
+    }
+}
+
+string CheckingIfElementExists(double[,] inArray, int row, int column)
+{
+    string result = $"элемента с позицией {row},{column} нет в массиве";
+    for (int i = 0; i < inArray.GetLength(0); i++)
+    {
+        for (int j = 0; j < inArray.GetLength(1); j++)
+        {
+            if (i == row - 1 && j == column - 1)
+            {
+                result = $"{row},{column} -> {inArray[i, j]}";
+                break;
+            }
+        }
+    }
+    return result;
+}
---
+++ Dz 52/Program.cs
@@ -0,0 +1,62 @@
+﻿int m = GetUserNumber("Введите число строк массива: ", "Ошибка ввода!");   
+int n = GetUserNumber("Введите число столбцов массива: ", "Ошибка ввода!");
+
+int[,] array = GetArray(m, n, -100, 100);       
+PrintArray(array);
+double[] result = average (array);
+Console.WriteLine("----------------------------------");
+Console.WriteLine(String.Join("; ", result));
+
+int GetUserNumber(string messageToUser, string errorMessage)
+{
+    while (true)
+    {
+        Console.Write(messageToUser);
+        if (int.TryParse(Console.ReadLine(), out int userNumber))
+            return userNumber;
+        Console.WriteLine(errorMessage);
+    }
+}
+
+int[,] GetArray (int m, int n, int minValue, int maxValue)
+{
+    int[,] resultArray = new int[m,n];
+    for (int i =0; i< m; i++)
+    {
+        for (int j=0; j < n; j++)
+        {
+           resultArray[i,j]=new Random().Next(minValue, maxValue + 1);
+           
+        }
+    }
+    return resultArray;
+}
+
+void PrintArray(int[,] inArray)
+{
+    for (int i = 0; i < inArray.GetLength(0); i++)
+    {
+        for (int j = 0; j < inArray.GetLength(1); j++)
+        {
+            Console.Write($"{inArray[i, j]}  ");
+        }
+        Console.WriteLine();
+    }
+}
+
+double[] average(int[,] inArray)
+{
+    double[] result = new double[inArray.GetLength(1)];
+    double count = 0;
+    for (int i = 0; i < inArray.GetLength(1); i++)
+    {
+        for (int j = 0; j < inArray.GetLength(0); j++)
+        {
+            count += (double)inArray[j, i];
+        }
+        result[i] = Math.Round((double)count / inArray.GetLength(0), 2);  
+
+        count = 0;
+    }
+    return result;
+}
